6913ceb4ced7dc46caf6025559fdbd029b59b821
MarxGentil <marxgentil@gmail.com>
2021-01-04 13:50:58 -0300

    Domain model

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
@@ -0,0 +1,115 @@
+package com.devsuperior.dsdeliver.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double price;
+	private String description;
+	private String imageUri;
+	
+	//construtor
+	public Product() {
+		
+	}
+	
+	//construtor com os argumentos
+	public Product(Long id, String name, Double price, String description, String imageUri) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.imageUri = imageUri;
+	}
+
+	//método para converter o objeto em String para imprimir por exemplo na tela.
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", description=" + description
+				+ ", imageUri=" + imageUri + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getImageUri() {
+		return imageUri;
+	}
+	
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+
+	// para comparar apenas pelo id um produto com outro. A própria IDE faz isso pelo source(botão direito do mouse)
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	// para comparar apenas pelo id um produto com outro. A própria IDE faz isso pelo source(botão direito do mouse)
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
