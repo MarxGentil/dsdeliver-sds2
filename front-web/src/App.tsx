0d4986765d885f8cfa6676712e05d6ff56a47b8d
MarxGentil <marxgentil@gmail.com>
2021-01-08 21:25:31 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,16 @@
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './Navbar';
 import Routes from './Routes';
 
 function App() {
   return (
     <div>
-      <Routes/>
+      <>
+        <Routes/>
+        <ToastContainer/>
+      </>
     </div>
   );
 }
